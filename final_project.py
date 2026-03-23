55c02663959b98361bd464aa7841f7da6bf768ee
carolina-rossman <rossman5@illinois.edu>
2026-03-23 17:46:33 -0500

    start of outline

--- final_project.py
+++ final_project.py
@@ -1,1 +1,6 @@
-text = "hi Alex hi"
+# Load/Design Images (Alex)
+
+# Start game Screen (Carolina)
+
+# Begginging sequence, Dino runs on screen, like in jetpack joyride, 
+# once it hits the center of the screen the background begins to scroll instead on the dino moving 

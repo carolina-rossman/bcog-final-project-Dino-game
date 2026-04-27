66402066a7372a55cf8ff5d54c959923f31f89f6
carolina-rossman <rossman5@illinois.edu>
2026-04-27 16:40:06 -0500

    updated working start_screen, need to make function with instructions_screen.py

--- src/start_screen.py
+++ src/start_screen.py
@@ -6,57 +6,56 @@ import instructions_screen
 import sys
 
 class Display(): 
-    screen_size = pygame.Surface((800, 600))
+    screen_size = (800, 600)
     def __init__(self):
         pygame.init()
         self.screen_size_x = self.screen_size[0]
         self.screen_size_y = self.screen_size [1]
-        self.canvas = pygame.display.set_mode(width = self.screen_size_x, height= self.screen_size_y)
-        self.background_image = pygame.image.load(file = "../stimuli/start_screen_background.png")
-        self.background_image = pygame.transform.scale(self.background_image, (self.screen_size_x, self.screen_size_y))
+        self.canvas = pygame.display.set_mode(self.screen_size)
+        self.background_image = pygame.image.load("../stimuli/start_screen_background.png")
+        self.background_image = pygame.transform.scale(self.background_image, (self.screen_size))
+        self.start_button = pygame.Rect(300, 250, 200, 50)
+        self.quit_button = pygame.Rect(300, 350, 200, 50)
         pygame.display.set_caption("Start Screen")
-        self.init_window()
-        self.create_interface_buttons()
 
     def init_window(self):
-       self.canvas.blit(self.background_image)
-       pygame.draw.rect(self.canvas)
-       self.title_font = pygame.font.SysFont("Arial", 60, bold = True)
-       title = self.title.render ("Dino Dash")
-       self.canvas.blit (title)
-       pygame.display.update()
+       self.canvas.blit(self.background_image, (0,0))
+       self.title_font = pygame.font.SysFont("Arial", 65, bold = True)
+       title = self.title_font.render("Dino Dash", True, (255, 255, 255))
+       title_rect = title.get_rect(center = (self.screen_size_x/2, 100))
+       self.canvas.blit(title, title_rect)
+
 
     def create_interface_buttons(self):
-        # quit button 
-        pygame.draw.rect(self.canvas, self.start_button)
-        pygame.draw.rect(self.canvas, self.quit_button)
+        pygame.draw.rect(self.canvas, (0, 200, 0), self.start_button)
+        pygame.draw.rect(self.canvas,(200, 0, 0), self.quit_button)
         self.button_font = pygame.font.SysFont("Arial", 20, bold=True)
-        start_text = self.button_font.render ("Start!", True)
-        quit_text = self.button_font.render ("Quit", True)
+        start_text = self.button_font.render ("Start!", True, (255, 255, 255))
+        quit_text = self.button_font.render ("Quit", True, (255, 255, 255))
         self.canvas.blit (start_text, (self.start_button.x, self.start_button.y))
         self.canvas.blit(quit_text, (self.quit_button.x, self.quit_button.y))
-        pygame.display.update()
+
     def go(self):
+        clock = pygame.time.Clock()
         running = True 
         while running: 
             self.init_window()
             self.create_interface_buttons()
             pygame.display.update()
-            for response in pygame.event.get(): 
-                if response.type == pygame.QUIT: 
+            for event in pygame.event.get(): 
+                if event.type == pygame.QUIT: 
                     pygame.quit()
-                    sys.exit()
-            
-                if response.type == pygame.MOUSEBUTTONDOWN: 
-                    mouse_pos = response.pos
-                    if self.start_button.collidepoint(mouse_pos):
+                    sys.exit()   
+                if event.type == pygame.MOUSEBUTTONDOWN: 
+                    print("Start clicked!")
+                    instructions_screen.Display.go()
+                    if self.start_button.collidepoint(event.pos):
                         instructions_screen.Display.go()
-                    if self.quit_button.collidepoint(mouse_pos): 
+                    if self.quit_button.collidepoint(event.pos): 
                         pygame.quit()
                         sys.exit()
-        instructions_screen.Display().run()
-        
-
+            pygame.display.update
+            clock.tick(120)
 
 def main(): 
     my_display = Display()

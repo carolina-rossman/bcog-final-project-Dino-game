c73c9738b9cad9f0b373941a332c30b02d321fcd
carolina-rossman <rossman5@illinois.edu>
2026-05-05 10:08:56 -0500

    start of spawning, obstacles, powerups/downs code

--- src/game.py
+++ src/game.py
@@ -19,22 +19,32 @@ class Display():
         self.jetpack = tk.PhotoImage(file = "../stimuli/jetpack_token.png")
         self.immunity = tk.PhotoImage(file = "../stimuli/shield_token.png")
         self.revival = tk.PhotoImage(file = "../stimuli/life_token.png")
-        selected_powerup = random.choice(self.jetpack, self.immunity, self.revival)
+        self.selected_powerup = random.choice(self.jetpack, self.immunity, self.revival)
 
     def create_powerdowns(self):
         self.speed_up = tk.PhotoImage(file = "../stimuli/double_time_token.png")
         self.tiny_dino = tk.PhotoImage(file = "../stimuli/tiny_dino_token.png")
-        selected_powerdown = random.choice(self.speed_up, self.tiny_dino)
+        self.selected_powerdown = random.choice(self.speed_up, self.tiny_dino)
         
     def create_obstacles(self): 
         self.fence = tk.PhotoImage(file= "../stimuli/fence.png")
         self.bush = tk.PhotoImage(file = "../stimuli/bush.png")
-        selected_obstacles = random.choice (self.bush, self.fence)
+        self.selected_obstacles = random.choice (self.bush, self.fence)
         # generate those images as background scrolls 
         # use pygame
-    def user_jumps(self): 
-        pass
 
+    def spawn_powerups(self):
+        power_ups = self.selected_powerup(800, random.randint(100,500))
+        power_ups.add(power_ups)
+    
+    def spawn_powerdown(self):
+        power_down = self.selected_powerdown(800, random.randint(100,500))
+        power_down.add(power_down)
+    
+    def spawn_obstacleds(self): 
+        obstacles = self.selected_obstacles(800, random.randint(100,500))
+        obstacles.add(obstacles)
+        
 
         
  
